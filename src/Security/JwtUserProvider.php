dd185da2ddbfb72a1d64ef00e28fe9f83cef472f
xjasmx <xjasmx@gmail.com>
2020-08-04 18:55:22 +0300

    up version

---
+++ src/Security/JwtUserProvider.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Jasmcoder\TicketingCommonBundle\Security;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUserInterface;
+
+class JwtUserProvider implements JWTUserInterface
+{
+    private string $username;
+
+    private array $roles;
+    private string $id;
+
+    public function __construct(string $username, string $id, array $roles = [])
+    {
+        $this->username = $username;
+        $this->roles = $roles;
+        $this->id = $id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function createFromPayload($username, array $payload)
+    {
+        if (isset($payload['roles'])) {
+            return new static($username, $payload['id'],  (array)$payload['roles']);
+        }
+
+        return new static($username, $payload['id']);
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUsername():string
+    {
+        return $this->username;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPassword()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSalt()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function eraseCredentials(): void
+    {
+    }
+}
