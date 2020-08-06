f3bf7f3bf35477d684e429cdba4ac1691b29bc68
xjasmx <xjasmx@gmail.com>
2020-08-06 15:38:33 +0300

    up version

--- src/Exception/AuthenticationException.php
+++ src/Exception/AuthenticationException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Jasmcoder\TicketingCommonBundle\Exception;
 
-class AuthorizationException extends \Exception
+class AuthenticationException extends \Exception
 {
     public function __construct(string $message, int $code = 0)
     {
